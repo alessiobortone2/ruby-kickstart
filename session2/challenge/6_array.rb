6f9cb16483b962f094ba77969aa4f1fba917efa9
alessiobortone2 <alessiobortone2@gmail.com>
2017-07-18 19:57:36 +0100

    Exercise 2:6 works with a little help from Stack Overflow

--- session2/challenge/6_array.rb
+++ session2/challenge/6_array.rb
@@ -1,16 +1,41 @@
 # Write a method named prime_chars? which takes array of strings
-# and returns true if the sum of the characters is prime. 
-# 
-# Remember that a number is prime if the only integers that can divide it with no remainder are 1 and itself. 
-# 
+# and returns true if the sum of the characters is prime.
+#
+# Remember that a number is prime if the only integers that can divide it with no remainder are 1 and itself.
+#
 # Examples of length three
 # prime_chars? ['abc']            # => true
 # prime_chars? ['a', 'bc']        # => true
 # prime_chars? ['ab', 'c']        # => true
 # prime_chars? ['a', 'b', 'c']    # => true
-# 
+#
 # Examples of length four
 # prime_chars? ['abcd']           # => false
 # prime_chars? ['ab', 'cd']       # => false
 # prime_chars? ['a', 'bcd']       # => false
 # prime_chars? ['a', 'b', 'cd']   # => false
+
+def prime_chars?(strings)
+  i = 0
+  strings.each { |x| i += x.length }
+  is_prime?(i)
+end
+
+def is_prime?(num)
+  return false if num <= 1
+  Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
+  true
+end
+
+
+#my original code - before I discovered this brilliant method on StackOverflow :)
+#c = 2
+#while c != i do
+#  if i % c = 0
+#    return true
+#  else
+#    c += 1
+#  end
+#end
+#return false
+#end
