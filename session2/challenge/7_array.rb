d7c18df278f63032945377b4a8ac1ef70c1d4d8f
alessiobortone2 <alessiobortone2@gmail.com>
2017-07-18 20:08:37 +0100

    Exercise 2:7 works, with a little help for the regex

--- session2/challenge/7_array.rb
+++ session2/challenge/7_array.rb
@@ -3,8 +3,15 @@
 # In order to not have to write an actual language parser, there won't be any punctuation too complex.
 # There will be no "'" that is not part of a contraction.
 # Assume each of these  charactsrs are not to be considered: ! @ $ # % ^ & * ( ) - = _ + [ ] : ; , . / < > ? \ |
-# 
+#
 # Examples
 # alternate_words("Lorem ipsum dolor sit amet.")  # => ["Lorem", "dolor", "amet"]
 # alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
 # alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
+
+def alternate_words(str)
+  '!@$#%^&*()-=_+[]:;,./<>?\\|'.split(//).each { |c| str = str.gsub(c, ' ') }  #with help for the Regex 
+  arr1 = str.split(' ')
+  arr2 = arr1.select.with_index { |x,i| i.even? }
+  return arr2
+end
