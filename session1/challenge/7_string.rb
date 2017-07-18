2f43bf0485c38b15ee4f0568e8abecbf729e3826
alessiobortone2 <alessiobortone2@gmail.com>
2017-07-18 16:43:21 +0100

    Exercise 1:7 with minor issue

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,13 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+result = ""
+  string.size.times { |i| result << string[i+1] if string[i] == "r" || string[i] == "R"}
+return result
 end
+
+# I really don't get the last example - every other case seems to work apart from this one
