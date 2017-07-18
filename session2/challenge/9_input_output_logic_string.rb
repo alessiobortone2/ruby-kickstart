229abc7e2b744fc64c2799a1ea881bb010a3ead2
alessiobortone2 <alessiobortone2@gmail.com>
2017-07-18 20:25:37 +0100

    Exercise 2:9 works - rake fails it due to no output in case of BYE, but there was no spec for it

--- session2/challenge/9_input_output_logic_string.rb
+++ session2/challenge/9_input_output_logic_string.rb
@@ -24,7 +24,18 @@
 # USER:     BYE
 
 def deaf_grandma
-
+  loop do
+    niece = gets.chomp
+    if niece == "BYE"
+      puts "BYE"
+      break
+    elsif niece == niece.upcase
+      puts "NO, NOT SINCE 1938!"
+    else
+      puts "HUH?! SPEAK UP, SONNY!"
+    end
+  end
+  exit 
 end
 
 
