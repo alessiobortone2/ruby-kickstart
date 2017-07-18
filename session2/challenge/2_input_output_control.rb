c53473af7e5d8059c0a739f24cf361e804550521
alessiobortone2 <alessiobortone2@gmail.com>
2017-07-18 17:52:20 +0100

    Exercise 2:2 works, even though Rake fails it - not sure why

--- session2/challenge/2_input_output_control.rb
+++ session2/challenge/2_input_output_control.rb
@@ -1,5 +1,5 @@
 # Prompt the user for a number, then read it in and print out "hi" that many times
-# 
+#
 # Repeat this process until the user submits "bye", then say "goodbye" and end the program
 # HINT: Check out example 2 if you get stuck
 
@@ -18,14 +18,23 @@
 # remember you can try your program out with              $ ruby 2_input_output_control.rb
 # and when you think it is correct, you can test it with  $ rake 2:2
 
-def hi_hi_goodbye
-  # your code here
+def hi_hi_goodbye(n)
+  puts "hi " * n
 end
 
-
-
+puts "Input number or bye"
+loop do
+  n = gets.chomp
+  if n == "bye"
+    puts "goodbye"
+    exit
+  elsif n.is_a? Integer
+    puts "Please input either an integer number or the word bye"
+  else
+    hi_hi_goodbye(n.to_i)
+  end
+end
 
 # This will just invoke the method if you run this program directly
-# This way you can try it out by running "$ ruby 2_input_output_control.rb" 
+# This way you can try it out by running "$ ruby 2_input_output_control.rb"
 # but it will still work for our tests
-hi_hi_goodbye if $0 == __FILE__
