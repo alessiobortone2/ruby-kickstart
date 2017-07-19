908bd7d22a3881fa63b2d3e225ef84d912d1e875
alessiobortone2 <alessiobortone2@gmail.com>
2017-07-19 11:16:19 +0100

    Exercise 3:3 so excited it works I am going to cry

--- session3/challenge/3_hashes.rb
+++ session3/challenge/3_hashes.rb
@@ -6,3 +6,14 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+
+def word_count(str1)
+  str1.downcase!
+  hash1 = {}
+  arr1 = str1.split(' ')
+  arr1.each do |x|
+    i = arr1.count(x)
+    hash1[x] = i
+    end
+  return hash1
+end
